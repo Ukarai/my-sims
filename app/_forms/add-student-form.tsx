254e496a919aea59d4d2dbe99c586d71073f6996
Ukarai <david.house97@yahoo.com>
2024-07-24 15:42:22 +0100

    Add students to class

--- app/_forms/add-student-form.tsx
+++ app/_forms/add-student-form.tsx
@@ -11,32 +11,43 @@ import {
 import { FormEvent, useState } from "react";
 import { createStudent } from "../actions";
 import { Class } from "@prisma/client";
+import { revalidatePath } from "next/cache";
+
+interface FormProps {
+  setShowForm: (a: boolean) => void;
+  classes?: Class[];
+  revalidateStudents: () => void;
+}
 
 const AddStudentForm = ({
-  students,
-  setStudents,
-  setShowAddStudentForm,
+  setShowForm,
   classes,
-}: any) => {
+  revalidateStudents,
+}: FormProps) => {
   const [firstName, setFirstName] = useState<string>("");
   const [surename, setSurename] = useState<string>("");
-  const [classId, setClassId] = useState<number>();
+  const [classId, setClassId] = useState<number>(-1);
 
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
 
-    const added = students;
-    const toAdd = await createStudent(firstName, surename, classId as number);
+    console.log(classes);
+    console.log(classId);
 
-    added.push(toAdd);
-    setStudents(added);
+    let cId: number =
+      classes !== undefined && classes?.length === 1 ? classes[0].id : classId;
+
+    await createStudent(firstName, surename, cId);
+
     setFirstName("");
     setSurename("");
-    setShowAddStudentForm(false);
+    setShowForm(false);
+    revalidateStudents();
   };
 
   return (
-    <form onSubmit={handleSubmit} className="m-4 flex flex-col gap-4">
+    <form onSubmit={handleSubmit} className="m-4 flex flex-col gap-4 max-w-fit">
+      <h2 className="mx-auto">Student Details</h2>
       <FormControl>
         <TextField
           className="border-2 "
@@ -58,22 +69,26 @@ const AddStudentForm = ({
           required
         />
       </FormControl>
-      <FormControl>
-        <InputLabel id="select-class-label">Class</InputLabel>
-        <Select
-          labelId="select-class-label"
-          label="Class"
-          value={classId as any}
-          autoWidth
-          onChange={(e: SelectChangeEvent) => setClassId(e.target.value as any)}
-        >
-          {classes.map((a: Class) => (
-            <MenuItem key={a.id} value={a.id}>
-              {a.name}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
+      {classes?.length !== undefined && classes?.length > 1 && (
+        <FormControl>
+          <InputLabel id="select-class-label">Class</InputLabel>
+          <Select
+            labelId="select-class-label"
+            label="Class"
+            value={classId as any}
+            autoWidth
+            onChange={(e: SelectChangeEvent) =>
+              setClassId(e.target.value as any)
+            }
+          >
+            {classes?.map((a: Class) => (
+              <MenuItem key={a.id} value={a.id}>
+                {a.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      )}
       <Stack direction="row" spacing={2}>
         <Button type="submit" variant="contained" className="bg-primary flex-1">
           Add
@@ -82,7 +97,7 @@ const AddStudentForm = ({
           variant="contained"
           color="error"
           className="flex-1"
-          onClick={() => setShowAddStudentForm(false)}
+          onClick={() => setShowForm(false)}
         >
           Cancel
         </Button>
--- app/class/[[...classId]]/page.tsx
+++ app/class/[[...classId]]/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import StudentTable from "@/app/_components/student-table";
+import AddStudentForm from "@/app/_forms/add-student-form";
 import { getAllClasses, getAllStudents, getClasses } from "@/app/actions";
 import { Button } from "@mui/material";
 import { Class, Student } from "@prisma/client";
@@ -13,6 +14,7 @@ export default function ClassPage({
 }) {
   const [classData, setClassData] = useState<Class[]>();
   const [students, setStudents] = useState<Student[]>();
+  const [showNewStudentForm, setShowNewStudentForm] = useState<Boolean>(false);
 
   useEffect(() => {
     async function load() {
@@ -35,6 +37,16 @@ export default function ClassPage({
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const revalidateStudents = async () => {
+    const studentData = await getAllStudents();
+
+    setStudents(studentData);
+  };
+
+  if (classData === undefined) {
+    return <div>Uh oh</div>;
+  }
+
   return (
     <div className="flex flex-col items-left flex-wrap ml-12 mt-4 gap-2">
       <div className="flex flex-row gap-2 text-3xl mx-auto">
@@ -52,9 +64,25 @@ export default function ClassPage({
           ).length
         }
       </div>
-      <Button variant="contained" className="max-w-fit bg-primary">
-        New Student
-      </Button>
+      {}
+
+      {showNewStudentForm ? (
+        <div className="max-w-fit border-primary border-solid border-2 rounded-xl">
+          <AddStudentForm
+            setShowForm={setShowNewStudentForm}
+            classes={classData}
+            revalidateStudents={revalidateStudents}
+          />
+        </div>
+      ) : (
+        <Button
+          variant="contained"
+          className="max-w-fit bg-primary"
+          onClick={() => setShowNewStudentForm(!showNewStudentForm)}
+        >
+          New Student
+        </Button>
+      )}
       <StudentTable
         students={students}
         classes={classData?.map((c: Class, i) => c.id)}
